a0ccb66c94031e95f3033b5cf804c80a1cc1d63a
최정문 <fourthdoor721@gmail.com>
2024-09-11 18:02:52 +0900

    구조고민중

---
+++ Assets/3.Script/Inventory/Slot_Inventory.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot_Inventory : MonoBehaviour
+{
+    [SerializeField] private Image _itemImage;
+    [SerializeField] private Text _amount;
+    [SerializeField] private float _durability;
+
+    private string 
+
+    private Text _displayitemName;
+    private Text _displayiteminfo;
+
+
+    private void OnMouseEnter()
+    {
+        _displayitemName =
+    }
+}
---
+++ Assets/3.Script/Item/Materials.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Materials : MonoBehaviour
+{    
+    public string name;
+    public string info;
+    public GameObject obj;
+    
+    
+}
+   
