7cd1f798e4bfd325416edccc89d49b9ab7a158d3
LeftMerona <fourthdoor721@gmail.com>
2024-10-02 00:49:42 +0900

    add water

--- Assets/3.Script/ChunkManager.cs
+++ Assets/3.Script/ChunkManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ChunkManager : MonoBehaviour
 {
     [SerializeField] GameObject oceanprefab;
-    private int planelength = 10;
+    private int planelength = 50;
     private int chunksize = 100;
     private int renderDistance = 50;
 
@@ -21,9 +21,9 @@ public class ChunkManager : MonoBehaviour
     private void LoadChunks()
     {
 
-        for(int i = -20; i < 20; i++)
+        for(int i = -5; i < 5; i++)
         {
-            for(int j = -20; j < 20; j++)
+            for(int j = -5; j < 5; j++)
             {
                 Vector3 chunkpos = new Vector3(playerpos.position.x + i * planelength, -0.5f, playerpos.position.y + j * planelength);
                 GameObject chunk = Instantiate(oceanprefab, chunkpos, Quaternion.identity);
